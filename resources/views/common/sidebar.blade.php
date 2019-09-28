3329f88de581662fa745fc3d9f1eda0d9e9929a8
mehrannasr1375 <mrparadox1397@gmail.com>
2019-09-28 14:41:10 +0330

    continuing dashboard

--- resources/views/common/sidebar.blade.php
+++ resources/views/common/sidebar.blade.php
@@ -4,7 +4,8 @@
 
     <!-- dashboard btn -->
     <a href="/" class="sidebar-btn">
-        <i class="fa fa-2x fa-dashboard text-light"></i>
+{{--        <i class="fa fa-2x fa-dashboard text-light"></i>--}}
+        <img src="{{ asset('images/icons/dashboard-1.png') }}" alt="">
         <p>پیشخوان</p>
     </a>
 
@@ -12,7 +13,8 @@
 
     <!-- add order btn -->
     <a href="/orders/create" class="sidebar-btn">
-        <i class="fa fa-2x fa-plus-circle text-light"></i>
+{{--        <i class="fa fa-2x fa-plus-circle text-light"></i>--}}
+        <img src="{{ asset('images/icons/add.png') }}" alt="">
         <p>افزودن تعمیری</p>
     </a>
 
@@ -20,7 +22,8 @@
 
     <!-- prepaired btn -->
     <a href="/prepaired" class="sidebar-btn">
-        <i class="fa fa-2x fa-check-circle text-light"></i>
+{{--        <i class="fa fa-2x fa-check-circle text-light"></i>--}}
+        <img src="{{ asset('images/icons/checkout-2.png') }}" alt="">
         <p>قطعات آماده تحویل</p>
     </a>
 
@@ -28,7 +31,8 @@
 
     <!-- repairing btn -->
     <a href="/repairing" class="sidebar-btn">
-        <i class="fa fa-2x fa-wrench text-light"></i>
+{{--        <i class="fa fa-2x fa-wrench text-light"></i>--}}
+        <img src="{{ asset('images/icons/repairing.png') }}" alt="">
         <p>قطعات در حال تعمیر</p>
     </a>
 
@@ -36,7 +40,8 @@
 
     <!-- customers btn -->
     <a href="/customers" class="sidebar-btn">
-        <i class="fa fa-2x fa-vcard-o text-light"></i>
+{{--        <i class="fa fa-2x fa-vcard-o text-light"></i>--}}
+        <img src="{{ asset('images/icons/customer.png') }}" alt="">
         <p>مشتریان</p>
     </a>
 
@@ -44,7 +49,8 @@
 
     <!-- history btn -->
     <a href="/orders" class="sidebar-btn">
-        <i class="fa fa-2x fa-clock-o text-light"></i>
+{{--        <i class="fa fa-2x fa-clock-o text-light"></i>--}}
+        <img src="{{ asset('images/icons/history.png') }}" alt="">
         <p>کل سابقه</p>
     </a>
 
@@ -54,11 +60,7 @@
     <a>
         <i class="fa fa-3x fa-angle-double-right text-light"></i>
     </a>
-    <div class="text-gold text-center text-vvsm">
-        <hr class="bg-gold my-0"/>
-        طراحی و توسعه از Mehran Nasr
-        <hr class="bg-gold my-0"/>
-    </div>
+
 
 
 
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -10,13 +10,45 @@
     <div class="row">
         <div class="col-7 dash-con">
             <div class="dash-con-inner">
-                
+                <div>
+                    <img src="{{ asset('/images/icons/message.png') }}" alt="">
+                    <p>پیام ها</p>
+                    <p>6 پیام</p>
+                </div>
+                <div>
+                    <table class="tbl-1">
+                        <tr>
+                            <th>#</th>
+                            <th>عیب</th>
+                            <th>عیب</th>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>a</td>
+                            <td>a</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>a</td>
+                            <td>a</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>a</td>
+                            <td>a</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
         <div class="col-5 dash-con">
             <div class="dash-con-inner">
-                   
-            </div>    
+                <div>
+                    <img src="{{ asset('/images/icons/calendar.png') }}" alt="">
+                    <p>تقویم</p>
+                    <p>امروز</p>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -24,8 +56,12 @@
     <div class="row">
         <div class="col dash-con">
             <div class="dash-con-inner">
-                 
-            </div> 
+                 <div>
+                     <img src="{{ asset('/images/icons/clock.png') }}" alt="">
+                     <p>زمان</p>
+                     <p>جاری</p>
+                 </div>
+            </div>
         </div>
     </div>
 
@@ -33,8 +69,12 @@
     <div class="row">
         <div class="col dash-con">
             <div class="dash-con-inner">
-                  
-            </div> 
+                <div>
+                    <img src="{{ asset('/images/icons/diagram.png') }}" alt="">
+                    <p>نمودار</p>
+                    <p>سود خالص ماهیانه</p>
+                </div>
+            </div>
         </div>
     </div>
 
